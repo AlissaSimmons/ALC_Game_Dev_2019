da81667dd1bf77c2d9c91f4a6db4d02753c4f862
Alissa Simmons <alissa.s180@stu.nebo.edu>
2018-09-21 10:29:11 -0600

    Added a Killbox and a score

---
+++ 2D Game/Assets/Scripts/KillBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other){
+
+		if(other.name == "Protagonist")
+		{
+		Debug.Log("Player Enters Death Zone");
+		Destroy(other);
+		}
+	
+	}
+}
---
+++ 2D Game/Assets/Scripts/Score Manager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+Text ScoreText;
+
+	// Use this for initialization
+	void Start () {
+		ScoreText = GetComponent<Text>();
+
+		Score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (score < 0)
+			Score = 0;
+
+			ScoreText.text = " " + Score;
+	}
+}
